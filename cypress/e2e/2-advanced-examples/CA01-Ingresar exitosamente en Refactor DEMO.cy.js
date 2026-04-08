2f6e93d657e528750f04bc0181f98e4a8e6abe10
marcosdmarin <marcos.marin@ticnow.cl>
2026-04-08 16:26:51 -0400

    Update CA01 elements

--- cypress/e2e/2-advanced-examples/CA01-Ingresar exitosamente en Refactor DEMO.cy.js
+++ cypress/e2e/2-advanced-examples/CA01-Ingresar exitosamente en Refactor DEMO.cy.js
@@ -1,15 +1,15 @@
 describe('CA01 - Ingresar exitosamente en Refactor DEMO', () => { //Nombre del Caso de Prueba Automático
 
 
-  //Validación de elementos en interfaz principal GitHub
+  //Validación de elementos en interfaz principal DEMO Refactor
   it('Validar interfaz de DEMO', () => {
     cy.visit('https://refactor.qa.idnowlab.com/security-services/demo/')
-    cy.get('[name="form.loginUserForm"] > .card-block > h1').should("be.visible")
-    //cy.get('h1').contains("Sign in to GitHub")           //Título de página Sign in to GitHub
+    cy.get('#btnStart').should("be.visible")
+    
 
   })
 
-  //Login GitHub
+  //Login DEMO Refactor
   it('Ingresar con credenciales validas en DEMO', () => {
     cy.visit('https://refactor.qa.idnowlab.com/security-services/demo/') 
     //cy.xpath('/html/body/ui-view/ui-view/div/div/div/div/div/div/form[1]/div/div[1]/input')           //URL login GitHub
@@ -20,13 +20,12 @@ describe('CA01 - Ingresar exitosamente en Refactor DEMO', () => { //Nombre del C
     //cy.get('#password').type("Md21156365")          //Input P
     //cy.get('.position-relative > .btn').click()     //Btn Sign in
 
-  //Ingreso exitoso en Home GitHub
-    cy.get('.AppHeader-context-item-label').should("be.visible")          //Texto Dashboard
-    cy.get('feed-container > :nth-child(1) > h3').contains("Home")        //Título Home
-    cy.get('.Button-label > .avatar').should("be.visible")                //Avatar Usuario
-    cy.get('.flex-justify-between.flex-items-center > :nth-child(2) > a').click() //Click en FB
-    cy.get('.AppHeader-context-item-label').contains("community")                //Página Community
-    
+  //Ingreso exitoso en DEMO Refactor
+    cy.get('h1').should("be.visible")          //Ticnow Identity
+    cy.get('h2').contains("One")        //Título One login for
+    cy.get('#txtLicense').should("be.visible")                //Licencia demo code
+    cy.get('#sdkLicense').should("be.visible")                //Licencia sdk web
+    cy.get('#btnStart').click() //Click en Start Secure Session
   })
      
      
